447b05590012048028067318b02b9f5b63c8ac9c
odde90 <43670486+odde90@users.noreply.github.com>
2019-11-13 18:21:59 +0100

    SpeechSynthesis.speak() to make api speak
    i used SpeechSynthesis.speak() from mozilla to make my api speak

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 const button = $(".trigger");
-
+var synth = window.speechSynthesis;
 button.click(function() {
   $.get("https://api.chucknorris.io/jokes/random", function(data) {
     console.log("response : ", data);
+    var utterThis = new SpeechSynthesisUtterance(data.value);
+    synth.speak(utterThis);
   });
 });
